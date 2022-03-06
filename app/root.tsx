7fccd87c5018ea98f5fd4db7b9d955958f116d27
Brendan Maclean <brendan.maclean@alumni.ubc.ca>
2022-03-06 16:58:49 -0700

    ESLint setup

--- app/root.tsx
+++ app/root.tsx
@@ -4,25 +4,23 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
+  ScrollRestoration,
 } from "remix";
 import type { MetaFunction } from "remix";
 
-export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
-};
+export const meta: MetaFunction = () => ({ title: "New Remix App" });
 
 export default function App() {
   return (
     <html lang="en">
-    <head>
-      <meta charSet="utf-8" />
-      <title>Remix: So great, it's funny!</title>
-    </head>
-    <body>
-      <h1></h1>
-      <LiveReload />
-    </body>
+      <head>
+        <meta charSet="utf-8" />
+        <title>Train HQ</title>
+      </head>
+      <body>
+        <h1>Train HQ</h1>
+        <LiveReload />
+      </body>
     </html>
   );
 }
