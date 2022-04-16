aac576c88e4b296a2049947026b2554eec2a8f70
Brendan Maclean <brendan.maclean@alumni.ubc.ca>
2022-04-16 12:57:12 -0600

    Initial Tailwind integration
    - installs dependencies
    - config files, stylelint config
    - inital app.css file & root import

--- app/root.tsx
+++ app/root.tsx
@@ -9,26 +9,14 @@ import {
 } from "remix";
 import type { MetaFunction, LinksFunction } from "remix";
 
-import globalStyles from "~/styles/global.css";
-import globalLargeStyles from "~/styles/global-large.css";
-import globalMedStyles from "~/styles/global-medium.css";
+import styles from "./styles/app.css";
 
 /**
  * CSS files can be cached long-term and your CSS is naturally code-split
  */
 export const links: LinksFunction = () => {
   return [
-    { rel: "stylesheet", href: globalStyles },
-    {
-      rel: "stylesheet",
-      href: globalLargeStyles,
-      media: "print, (min-width: 1024px)",
-    },
-    {
-      rel: "stylesheet",
-      href: globalMedStyles,
-      media: "print, (min-width: 640px)",
-    },
+    { rel: "stylesheet", href: styles },
     {
       rel: "stylesheet",
       href: "https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&display=swap",
@@ -65,6 +53,7 @@ function Document({
         <title>{title}</title>
         <Links />
         <Scripts />
+        <ScrollRestoration />
       </head>
       <body>
         {children}
